e4bb6e694823f4b9020f935d742b84f7996d502a
Shipra Verma <vaaishpra19@gmail.com>
2021-01-11 20:34:17 +0530

    new endpoints created

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -17,6 +17,7 @@ export default class Homepage extends Component {
         this.state = {
             roomCode: null,
         };
+        this.clearRoomCode = this.clearRoomCode.bind(this);
     }
 
     async componentDidMount() {
@@ -47,6 +48,13 @@ export default class Homepage extends Component {
             </Grid>
         );
     }
+
+    clearRoomCode() {
+        this.setState({
+          roomCode: null,
+        });
+      }
+
     render() {
         return <Router>
             <Switch>
@@ -57,10 +65,15 @@ export default class Homepage extends Component {
                     ) : (
                       this.renderHomepage()
                     );
-                  }}></Route>
+                  }}
+                  />
                 <Route path='/join' component={RoomJoinpage} />
                 <Route path='/create' component={CreateRoompage} />
-                <Route path='/room/:roomCode' component={Room} />
+                <Route path='/room/:roomCode' 
+                render={(props) => {
+                    return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+                  }}
+                  />
             </Switch>
         </Router>;
     }
--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,4 +1,5 @@
 import React,{ Component } from 'react';
+import { Grid, Button, Typography } from "@material-ui/core";
 
 export default class Room extends Component{
     constructor(props){
@@ -10,26 +11,72 @@ export default class Room extends Component{
         };
         this.roomCode = this.props.match.params.roomCode;
         this.getRoomDetails();
+        this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
     }
 
     getRoomDetails() {
         fetch("/api/get-room" + "?code=" + this.roomCode)
           .then((response) => response.json())
-          .then((data) => {
-            this.setState({
-              votes_to_skip: data.votes_to_skip,
-              guest_can_pause: data.guest_can_pause,
-              is_host: data.is_host,
-            });
+          .then((data) =>  {
+            if (!response.ok) {
+            this.props.leaveRoomCallback();
+            this.props.history.push("/");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          this.setState({
+            votes_to_skip: data.votes_to_skip,
+            guest_can_pause: data.guest_can_pause,
+            is_host: data.is_host,
           });
+        });
       }
+    
+    leaveButtonPressed(){
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      };
+      fetch("/api/leave-room", requestOptions).then((_response) => {
+        this.props.leaveRoomCallback();
+        this.props.history.push("/");
+      });
+    }  
 
     render(){
-        return <div>
-            <h3>{this.roomCode}</h3>
-            <p>Votes: {this.state.votes_to_skip}</p>
-            <p>Guest can pause: {this.state.guest_can_pause.toString()}</p>
-            <p>Host: {this.state.is_host.toString()}</p>
-        </div>
+        return (
+        <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" component="h4">
+            Code: {this.roomCode}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Votes: {this.state.votes_to_skip}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Guest Can Pause: {this.state.guest_can_pause.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Host: {this.state.is_host.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={this.leaveButtonPressed}
+          >
+            Leave Room
+          </Button>
+        </Grid>
+      </Grid>
+      );
     }
 }
